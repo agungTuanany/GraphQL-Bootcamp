8437db4b7d4c849a4305531c05c3f122346a9436
agungTuanany <agung.tuanany@gmail.com>
2019-05-21 04:20:15 +0700

    Client-side | Setup React and Book list components

---
+++ client/src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+import Booklist from './components/Booklist';
+
+const App = () => {
+  return (
+    <div className="App">
+      <h1> GraphQL BOOTCAMP</h1>
+      <Booklist />
+    </div>
+  );
+}
+
+export default App;
---
+++ client/src/components/Booklist.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Booklist = () => {
+  return (
+    <div className="bookList">
+      <ul>
+        <li>Book List</li>
+      </ul>
+    </div>
+  );
+}
+
+export default Booklist;
