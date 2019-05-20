d4631d79cb7dbc0484d546bccb73231c0cfad043
agungTuanany <agung.tuanany@gmail.com>
2019-05-20 23:05:20 +0700

    Connecting to mLab

--- server/app.js
+++ server/app.js
@@ -1,9 +1,19 @@
 const express = require('express');
 const graphqlHTTP = require('express-graphql');
-const schema = require('./schema/schema')
+const schema = require('./schema/schema');
 
+const mongoose = require('mongoose');
+const keys = require('./config/keys');
 
 const app = express();
+
+mongoose.connect(keys.mongoURI , { useNewUrlParser: true  });
+mongoose.connection.once('open', () => {
+  console.log('connected to database');
+})
+
+// connect to mlab databse
+// make sure to replace my db string and creds with your own
 
 app.use('/graphql', graphqlHTTP({
   schema,
@@ -11,6 +21,8 @@ app.use('/graphql', graphqlHTTP({
 
 }));
 
-app.listen(4000,() => {
-  console.log('now listening for request on port 4000');
+const PORT = process.env.PORT || 4000;
+
+app.listen(PORT,() => {
+  console.log(`now listening for request on port ${PORT}`);
 });
