ab484241cd20d06b495bfd2fcb45618e9b31d409
agungTuanany <agung.tuanany@gmail.com>
2019-05-21 16:02:22 +0700

    Updating Component State

--- client/src/components/AddBook.js
+++ client/src/components/AddBook.js
@@ -6,6 +6,18 @@ import { getAuthorsQuery } from '../queries/queries';
 
 class AddBook extends Component {
 
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      name:"",
+      genre:"",
+      authorId: "",
+    };
+
+    this.submitForm = this.submitForm.bind(this);
+  }
+
   displayAuthors() {
     const data = this.props.data;
     if(data.loading) {
@@ -13,26 +25,39 @@ class AddBook extends Component {
     }else {
       return data.authors.map(author => {
         return ( <option key={author.id} value={author.id}>{author.name}</option> );
-      })
+      });
     }
+  };
+
+  childOnChange(e) {
+    this.setState({
+      name: e.target.value,
+      genre: e.target.value,
+      authorId: e.target.value
+    });
+  };
+
+  submitForm(e) {
+    e.preventDefault();
+    console.log(this.state)
   }
 
   render() {
     return (
-      <form id="add-book">
+      <form id="add-book" onSubmit={this.submitForm}>
         <div className="field">
           <label>Book name:</label>
-          <input type="text" />
+          <input type="text" onChange={(e)=>this.setState({name:e.target.value})} />
         </div>
 
         <div className="field">
           <label>Genre:</label>
-          <input type="text" />
+          <input type="text" onChange={(e)=>this.setState({genre: e.target.value})}/>
         </div>
 
         <div className="field">
           <label>Author:</label>
-          <select>
+          <select onChange={this.childOnChange.authorId}>
             <option>Select author</option>
             { this.displayAuthors() }
           </select>
